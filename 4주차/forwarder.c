3ad7fb9129e0a91e5fa86154f36c0d042809890a
unknown <dlrnjsgud322@gmail.com>
2018-10-17 18:54:13 +0900

    feat: Complete week 4 report

---
+++ 4주차/forwarder.c
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2007, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *         Testing the broadcast layer in Rime
+ * \author
+ *         Adam Dunkels <adam@sics.se>
+ */
+
+#include "contiki.h"
+#include "net/rime.h"
+#include "random.h"
+
+#include "dev/button-sensor.h"
+
+#include "dev/leds.h"
+#include "dev/light-sensor.h"
+
+#include "dev/cc2420.h"
+#include "sys/node-id.h"
+
+#include <stdio.h>
+#include <string.h>
+
+struct message {
+	uint16_t src;
+	uint16_t dst;
+	uint16_t seqno;
+	uint16_t ttl;
+};
+static uint16_t last_seqno = 0;
+
+/*---------------------------------------------------------------------------*/
+PROCESS(example_broadcast_process, "Broadcast example");
+AUTOSTART_PROCESSES(&example_broadcast_process);
+/*---------------------------------------------------------------------------*/
+
+static void
+broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from)
+{
+  struct message msg;
+  memcpy(&msg, packetbuf_dataptr(), sizeof(struct message));
+
+  printf("message received from %d. seqno: %d, src: %d, dst: %d, ttl: %d\n", from->u8[0], msg.seqno, msg.src, msg.dst, msg.ttl);
+
+  // filtering for restriction.
+	if (msg.src == node_id ) 
+    return;
+  if (msg.seqno <= last_seqno) 
+    return;
+  if (msg.ttl <= 0) 
+    return;
+
+  // if all true, then forward packet.
+  last_seqno = msg.seqno;
+  msg.ttl--;
+  packetbuf_copyfrom(&msg, sizeof(struct message));
+  process_post(&example_broadcast_process, PROCESS_EVENT_CONTINUE, NULL);
+}
+static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+static struct broadcast_conn broadcast;
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(example_broadcast_process, ev, data)
+{
+  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+
+  PROCESS_BEGIN();
+
+  broadcast_open(&broadcast, 129, &broadcast_call);
+
+  while(1) {
+		// wait until callback function's wakeup call
+		PROCESS_WAIT_EVENT();
+
+    // send msg
+    broadcast_send(&broadcast);
+    printf("message forwarded.\n");
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
